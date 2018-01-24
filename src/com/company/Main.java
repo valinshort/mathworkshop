500e127e198358cc3184052736cdb385d0a80f9f
Valin Short <valin.short1998@gmail.com>
2018-01-24 15:24:39 -0500

    updating files

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,34 @@
 package com.company;
 
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+       // Math.pow();
+        System.out.println("Math.pow(3, 5) = " + Math.pow(3,5 ));
+                System.out.println(10.0 / 7);
+
+        System.out.println(" 5 * Math.pow(2 , 5)- 4 = " + (5 * Math.pow(2, 5 )-4));
+
+System.out.println(10.3 % 5);
+
+
+Math.random();
+System.out.println("Math.random() Examples:");
+        System.out.println("Math.random() * 10 = " + Math.random());
+
+        System.out.println("Math.ceil(Math.random() * 10) = "+ Math.ceil(+ Math.random()* 10));
+
+        System.out.println("Math floor( Examples");
+        System.out.println("Math.floor(10.3*5) = " + Math.floor(10.3 * 5);
+
+        System.out.println("Math.round()"); Math.round():
+        System.out.println("Math.rounds()) = " +
+
+
+
+
+
     }
 }
